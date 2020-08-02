b60b1aea10a90b6e70ea1d9a74c71aeb43648995
samuelyapwoonpin <samuelywp@gmail.com>
2020-08-03 05:15:28 +0800

    chore: add solution to fundamentals/c/ex5

---
+++ 01-fundamentals/02-c/e05-binary.c
@@ -0,0 +1,27 @@
+// Write a function binary(int x) that prints out the binary equivalent of a
+// number.
+
+#include <math.h>   // pow, round
+#include <stdio.h>  // printf, scanf
+
+int binary(int num);
+
+int main(void) {
+    printf("enter a positive integer: ");
+    int num;
+    scanf("%d", &num);
+    printf("the binary representation of %d is %d", num, binary(num));
+    return 0;
+}
+
+// Returns the binary representation of a (positive) integer.
+int binary(int num) {
+    int result = 0;
+    for (int exponent = 0; num > 0; num /= 2, ++exponent) {
+        int remainder = num % 2;
+        if (remainder == 1) {
+            result += round(pow(10, exponent));  // prevents truncation of fp
+        }
+    }
+    return result;
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Algorithms in C
+
+Solutions to exercises from the book Algorithms in C by Robert Sedgewick.
+
+## Authors
+
+- **Samuel Yap** - _Initial work_ - [imsamuel](https://github.com/imsamuel)
